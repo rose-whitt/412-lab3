9d933827b6f2593ce86bbdbcf8af3d5c9a554f62
rose-whitt <rew9@rice.edu>
2023-11-06 23:20:25 -0600

    END OF NIGHT- made pwhat i think si the graph data structure. shoudl add like a check dependence function and then start building the graph. i am so strong. gn

--- DP_MAP.py
+++ DP_MAP.py
@@ -27,15 +27,16 @@ class Edge:
         self.vr = None      # type- INT; Which value is flowing on the edge; Only defined if kind is Data
         
         # bidirectional
-        self.parent = None  # type- Node; where the edge is coming OUT OF
+        self.parent = None  # type- OperationNode; where the edge is coming OUT OF
         self.outof_line_num = None  # line num of parent node
-        self.child = None   # type- Node; where the edge is going INTO (i.e. the side that has the arrow)
+        self.child = None   # type- OperationNode; where the edge is going INTO (i.e. the side that has the arrow)
         self.into_line_num = None   # line num of child node
 
 
-class Node:
+class OperationNode:
     def __init__(self):
         self.line_num = None  # type- INT
+        self.ir_list_node = None # type- Node in IR_List.py; reference to the node in the IR_List that corresponds to this operation node
         self.type = None    # opcode
         self.delay = None     # latency of that opcode
         self.into_edges = []       # array of Edges going INTO this node
@@ -60,6 +61,7 @@ class DependenceGraph:
         # map line num of node that the edge is coming OUT OF (parent) to the edge; contains type Edge
         self.edges = {} 
         self.kinds = ["Data", "Serial", "Conflict"]
+        self.opcodes_list = ["load", "store", "loadI", "add", "sub", "mult", "lshift", "rshift", "output", "nop"]
     
     def add_node(self, node):
         """
@@ -75,12 +77,59 @@ class DependenceGraph:
         """
         self.edges[edge.outof_line_num] = edge
     
+    def get_ir_node(self, node):
+        # print(start)
+        lh = ""
+        rh = ""
+        
+        if (node.opcode == 0 or node.opcode == 1): # MEMOP
+            lh = "r" + str(node.arg1[VR_IDX])
+        elif (node.opcode == 2): # LOADI
+            lh = str(node.arg1[SR_IDX])
+        elif (node.opcode >= 3 and node.opcode <= 7):  # ARITHOP
+            lh = "r" + str(node.arg1[VR_IDX]) + ",r" + str(node.arg2[VR_IDX]) 
+        elif (node.opcode == 8): # OUTPUT
+            lh = str(node.arg1[SR_IDX])
+        
+        if (node.opcode != 8):
+            rh = "=> r" + str(node.arg3[VR_IDX])
+
+        opcode = self.opcodes_list[node.opcode] + " "
+
+        temp = opcode + lh + " " + rh
+        return temp
+    
+    def print_dg_map(self):
+        print("digraph DG {")
+        self.print_nodes()
+        self.print_edges()
+        print("}")
+
+    def print_nodes(self):
+        """
+            format:   1 [label="1:  loadI  8 => r3"];
+        """
+        ret = ""
+        for key, value in self.nodes:
+            temp = "  " # indent
+            temp += str(key)
+            temp += ' [ label="'
+            temp += str(value.line_num)
+            temp += ":  "
+            poo = self.get_ir_node(value.ir_list_node)
+            temp += poo
+            temp += ' "];'
+            temp += '\n'
+            ret += temp
+        print(ret)
+        
+
+    
     def print_edges(self):
         """
             format of each node:   3 -> 1 [ label=" Data, vr3"];
               7 -> 6 [ label=" Conflict "];
             <line num of node edge is coming OUT OF (parent)> -> <line num of node edge is going INTO (child)> [ label = " <kind> "];
-
         """
         ret = ""
         for key, value in self.edges:
@@ -99,6 +148,7 @@ class DependenceGraph:
                 temp += ' "];'
             temp += '\n'
             ret += temp
+        print(ret)
 
             
             
